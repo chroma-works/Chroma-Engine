425493d074e135aff48a5fd00b876d1a2afd83fe
Alper Şahıstan <alpersahistan@gmail.com>
2019-06-10 22:35:36 +0300

    Add test code for abstractions

--- src/chroma/main/main.cpp
+++ src/chroma/main/main.cpp
@@ -1,64 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 
-#include <thirdparty/glad/include/glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "chroma/window/Window.h"
 
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-int main(int argc, char const* argv[]) {
-    std::cout << "Chroma-Engine" << std::endl;
-
-    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
-
-    // Init GLFW
-    glfwInit();
-    // Set all the required options for GLFW
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-
-    // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Chroma", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    if (!window)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    // Set the required callback functions
-    glfwSetKeyCallback(window, key_callback);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize OpenGL context" << std::endl;
-        return -1;
-    }
-
-    // Define the viewport dimensions
-    glViewport(0, 0, 640, 480);
+int main(int argc, char const* argv[]) 
+{
+    Chroma::WindowProps props = Chroma::WindowProps();
+    Chroma::Window* window = new Chroma::Window(props);
 
     // Main loop
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window->m_window_handle))
     {
-        // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
-        glfwPollEvents();
-
-        // Render
-        // Clear the colorbuffer
-        glClearColor(0.184f, 0.062f, 0.129f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Swap the screen buffers
-        glfwSwapBuffers(window);
+        //glClearColor(0.184f, 0.062f, 0.129f, 1.0f);
+        window->OnUpdate();
     }
-
-    // Terminates GLFW, clearing any resources allocated by GLFW.
-    glfwTerminate();
 
     return 0;
 }
