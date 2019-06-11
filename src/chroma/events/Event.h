59a406de20e16ef66da81009fa78dc7d617e3b63
Alper Şahıstan <alpersahistan@gmail.com>
2019-06-11 18:21:31 +0300

    Implement Event

---
+++ src/chroma/events/Event.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+
+// from: https://github.com/TheCherno/Hazel/blob/master/Hazel/src/Hazel/Events/Event.h
+namespace Chroma
+{
+    #define BIT(x) (1 << x)
+    //Events are blocking be aware
+    enum class EventType
+    {
+        None = 0,
+        WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+        AppTick, AppUpdate, AppRender,
+        KeyPressed, KeyReleased, KeyTyped,
+        MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+    };
+
+    //Bitwise shifted enums since an event may belong to two or more category
+    enum EventCategory
+    {
+        None = 0,
+        EventCategoryApplication = BIT(0),
+        EventCategoryInput = BIT(1),
+        EventCategoryKeyboard = BIT(2),
+        EventCategoryMouse = BIT(3),
+        EventCategoryMouseButton = BIT(4)
+    };
+
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+    class Event
+    {
+    public:
+        bool handled = false;
+
+        virtual EventType GetEventType() const = 0;
+        virtual const char* GetName() const = 0;
+        virtual int GetCategoryFlags() const = 0;
+        virtual std::string ToString() const { return GetName(); }
+
+        inline bool IsInCategory(EventCategory category)
+        {
+            return GetCategoryFlags() & category;
+        }
+    };
+
+    class EventDispatcher
+    {
+        template<typename T>
+        using EventFn = std::function<bool(T&)>;
+    public:
+        EventDispatcher(Event& event)
+            : m_event(event)
+        {
+        }
+
+        template<typename T>
+        bool Dispatch(EventFn<T> func)
+        {
+            if (m_event.GetEventType() == T::GetStaticType())
+            {
+                m_event.handled = func(*(T*)&m_event);
+                return true;
+            }
+            return false;
+        }
+    private:
+        Event& m_event;
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const Event& e)
+    {
+        return os << e.ToString();
+    }
+}
