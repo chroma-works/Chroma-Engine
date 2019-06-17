bbb41e1ceab9550ddd10b81cb0eb95f660d9cc1e
Alper Şahıstan <alpersahistan@gmail.com>
2019-06-17 04:41:46 +0300

    Integrate Application class

--- src/chroma/main/main.cpp
+++ src/chroma/main/main.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "chroma/window/Window.h"
 #include <chroma/main/Log.h>
+#include <chroma/main/Application.h>
 
 int main(int argc, char const* argv[]) 
 {
     Chroma::Log::Init();
-    Chroma::WindowProps props = Chroma::WindowProps();
-    Chroma::Window* window = new Chroma::Window(props);
+    Chroma::Application* app = new Chroma::Application();
 
-    // Main loop
-    while (!glfwWindowShouldClose(window->m_window_handle))
-    {
-        //glClearColor(0.184f, 0.062f, 0.129f, 1.0f);
-        window->OnUpdate();
-    }
-
+    app->Run();
     return 0;
 }
