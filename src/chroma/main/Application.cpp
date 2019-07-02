be85edf30316b203d68beaff3284fb9c7bfc3d43
Alper Şahıstan <alpersahistan@gmail.com>
2019-07-02 04:28:47 +0300

    Integrate buffer to test

--- src/chroma/main/Application.cpp
+++ src/chroma/main/Application.cpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include <thirdparty/glad/include/glad/glad.h>
+
 #include "Application.h"
+
 #include <chroma/main/Log.h>
 #include <chroma/renderer/Shader.h>
+#include <chroma/openGL/OpenGLBuffer.h>
+
 #include <fstream>
 
 namespace Chroma
@@ -31,20 +35,14 @@ namespace Chroma
     {
         glClearColor(0.184f, 0.062f, 0.129f, 1.0f);
         // An array of 3 vectors which represents 3 vertices
-        static const GLfloat g_vertex_buffer_data[] = {
+        static const float g_vertex_buffer_data[] = {
             -1.0f, -1.0f, 0.0f,
             1.0f, -1.0f, 0.0f,
             0.0f,  1.0f, 0.0f,
         };
 
-        // This will identify our vertex buffer
-        GLuint vertexbuffer;
-        // Generate 1 buffer, put the resulting identifier in vertexbuffer
-        glGenBuffers(1, &vertexbuffer);
-        // The following commands will talk about our 'vertexbuffer' buffer
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        // Give our vertices to OpenGL.
-        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+        //vertex buffer
+        OpenGLVertexBuffer vertex_buffer((void*)&g_vertex_buffer_data, sizeof(g_vertex_buffer_data));
 
         // Create and compile our GLSL program from the shaders
         Shader shader("../assets/shaders/vs.shader", "../assets/shaders/fs.shader", Shader::READ_FILE_PATH);
@@ -56,7 +54,8 @@ namespace Chroma
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             // 1st attribute buffer : vertices
             glEnableVertexAttribArray(0);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+            //glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+            vertex_buffer.Bind();
             glVertexAttribPointer(
                 0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
                 3,                  // size
