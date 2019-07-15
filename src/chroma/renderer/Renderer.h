19019e836ba81634bda9a468d3cb8cc05d029f80
Alper Şahıstan <alpersahistan@gmail.com>
2019-07-16 02:33:53 +0300

    Change minor details

---
+++ src/chroma/renderer/Renderer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace Chroma
+{
+    class Renderer
+    {
+    public:
+        Renderer();
+        ~Renderer();
+
+        void Render();
+
+    private:
+
+    };
+
+    Renderer::Renderer()
+    {
+    }
+
+    Renderer::~Renderer()
+    {
+    }
+}
