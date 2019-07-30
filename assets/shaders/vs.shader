8c90dce2be6e1df7e58265333d2b3bfebe0127ee
Alper Şahıstan <alpersahistan@gmail.com>
2019-07-31 00:59:21 +0300

    Integrate test

--- assets/shaders/vs.shader
+++ assets/shaders/vs.shader
@@ -1,5 +1,5 @@
 #version 450 core
-layout(location = 0) in vec4 in_Position;
+layout(location = 0) in vec3 in_Position;
 layout(location = 1) in vec3 in_Color;
 out vec3 fragmentColor;
 
@@ -7,6 +7,7 @@ uniform mat4 u_Model, u_View, u_Proj;
 
 void main(){
 	  mat4 u_MVP = u_Proj * u_View * u_Model;
-	  gl_Position = u_MVP * in_Position;
-	  fragmentColor = in_Color;
+	  gl_Position = u_MVP * vec4(in_Position.xyz, 1.0f);
+	  //fragmentColor = in_Color;
+	  fragmentColor = vec3(0.0f, 0.0f, 1.0f);
 }
--- src/chroma/main/Application.cpp
+++ src/chroma/main/Application.cpp
@@ -4,6 +4,7 @@
 
 #include "Application.h"
 
+#include <chroma/main/AssetImporter.h>
 #include <chroma/main/Log.h>
 #include <chroma/openGL/OpenGLBuffer.h>
 #include <chroma/openGL/OpenGLVertexArrayObject.h>
@@ -11,9 +12,9 @@
 #include <chroma/renderer/Camera.h>
 
 #include <fstream>
+#include <string>
 #include <thirdparty/glm/glm/glm.hpp>
 #include <thirdparty/glm//glm/gtc/matrix_transform.hpp>
-#include <thirdparty/tinyobjloader/tiny_obj_loader.h>
 
 
 
@@ -45,13 +46,22 @@ namespace Chroma
 
         //cube
         // vertex position array
-        GLfloat vertices[] = {
+        /*GLfloat vertices[] = {
              .5f, .5f, .5f, 1.0f,  -.5f, .5f, .5f, 1.0f,  -.5f,-.5f, .5f, 1.0f,  .5f,-.5f, .5f, 1.0f,// v0,v1,v2,v3 (front)
              .5f, .5f, .5f, 1.0f,  .5f,-.5f, .5f, 1.0f,  .5f,-.5f,-.5f, 1.0f,  .5f, .5f,-.5f, 1.0f,// v0,v3,v4,v5 (right)
              .5f, .5f, .5f, 1.0f,  .5f, .5f,-.5f, 1.0f,  -.5f, .5f,-.5f, 1.0f,  -.5f, .5f, .5f, 1.0f,// v0,v5,v6,v1 (top)
             -.5f, .5f, .5f, 1.0f,  -.5f, .5f,-.5f, 1.0f, -.5f,-.5f,-.5f, 1.0f, -.5f,-.5f, .5f, 1.0f,// v1,v6,v7,v2 (left)
             -.5f,-.5f,-.5f, 1.0f,  .5f,-.5f,-.5f, 1.0f,  .5f,-.5f, .5f, 1.0f, -.5f,-.5f, .5f, 1.0f,// v7,v4,v3,v2 (bottom)
              .5f,-.5f,-.5f, 1.0f, -.5f,-.5f,-.5f, 1.0f,  -.5f, .5f,-.5f, 1.0f,  .5f, .5f,-.5f, 1.0f  // v4,v7,v6,v5 (back)
+        };*/
+
+        GLfloat vertices[] = {
+             .5f, .5f, .5f,    -.5f, .5f, .5f,    -.5f,-.5f, .5f,    .5f,-.5f, .5f,  // v0,v1,v2,v3 (front)
+             .5f, .5f, .5f,    .5f,-.5f, .5f,    .5f,-.5f,-.5f,    .5f, .5f,-.5f,  // v0,v3,v4,v5 (right)
+             .5f, .5f, .5f,    .5f, .5f,-.5f,    -.5f, .5f,-.5f,    -.5f, .5f, .5f,  // v0,v5,v6,v1 (top)
+            -.5f, .5f, .5f,    -.5f, .5f,-.5f,   -.5f,-.5f,-.5f,   -.5f,-.5f, .5f,  // v1,v6,v7,v2 (left)
+            -.5f,-.5f,-.5f,    .5f,-.5f,-.5f,    .5f,-.5f, .5f,   -.5f,-.5f, .5f,  // v7,v4,v3,v2 (bottom)
+             .5f,-.5f,-.5f,   -.5f,-.5f,-.5f,    -.5f, .5f,-.5f,    .5f, .5f,-.5f, 1.0f  // v4,v7,v6,v5 (back)
         };
 
         // normal array
@@ -93,49 +103,64 @@ namespace Chroma
             20,21,22,  22,23,20     // v4-v7-v6, v6-v5-v4 (back)
         };
 
+        /*GLuint indices[] = {
+             0, 1, 2,  2, 3, 0,    // v0-v1-v2, v2-v3-v0 (front)
+             4, 5, 6,  6, 7, 4,    // v0-v3-v4, v4-v5-v0 (right)
+             0, 3, 5,  5, 4, 0,    // v0-v5-v6, v6-v1-v0 (top)
+             3, 2, 6,  6, 5, 3,    // v1-v6-v7, v7-v2-v1 (left)
+             2, 1, 7,  7, 6, 2,    // v7-v4-v3, v3-v2-v7 (bottom)
+             1, 0, 4,  4, 7, 1     // v4-v7-v6, v6-v5-v4 (back)
+        };*/
+
         // Create and compile our GLSL program from the shaders
         Shader* shader = Shader::ReadAndBuildShaderFromFile("../assets/shaders/vs.shader", "../assets/shaders/fs.shader");
 
-        //Vertex positions buffer
-        OpenGLVertexBuffer* vertex_buffer = new OpenGLVertexBuffer((void*)&vertices, sizeof(vertices) * sizeof(GLfloat));
+        //Model import test
+        Mesh* mesh = AssetImporter::LoadMeshFromOBJ("../assets/models/monkey.obj");
 
-        VertexAttribute layout_attribute("in_Position", 0, ShaderDataType::Float4, GL_FALSE);
+        //Vertex positions buffer
+        OpenGLVertexBuffer* vertex_buffer = new OpenGLVertexBuffer((void*)mesh->m_vertex_positions.data(), 
+            mesh->m_vertex_positions.size() * sizeof(GLfloat) * 3);
+
+        VertexAttribute layout_attribute("in_Position", 0, ShaderDataType::Float3, GL_FALSE);
         VertexBufferLayout vertex_buffer_layout;
         vertex_buffer_layout.PushAttribute(layout_attribute);
         vertex_buffer->SetBufferLayout(vertex_buffer_layout);
 
         //Vertex colors buffer
-        OpenGLVertexBuffer* color_buffer = new OpenGLVertexBuffer((void*)&colors, sizeof(colors) * sizeof(GLfloat));
+        OpenGLVertexBuffer* normal_buffer = new OpenGLVertexBuffer((void*)mesh->m_vertex_normals.data(),
+            mesh->m_vertex_normals.size() * sizeof(GLfloat) * 3);
 
-        VertexAttribute layout_attribute2("in_Color", 1, ShaderDataType::Float3, GL_FALSE);
+        VertexAttribute layout_attribute2("in_Normal", 1, ShaderDataType::Float3, GL_FALSE);
         VertexBufferLayout vertex_buffer_layout2;
         vertex_buffer_layout2.PushAttribute(layout_attribute2);
-        color_buffer->SetBufferLayout(vertex_buffer_layout2);
+        normal_buffer->SetBufferLayout(vertex_buffer_layout2);
 
         //index buffer
-        OpenGLIndexBuffer* index_buffer = new OpenGLIndexBuffer((void*)&indices, sizeof(indices));
+        OpenGLIndexBuffer* index_buffer = new OpenGLIndexBuffer(mesh->m_indices.data(),mesh->m_indices.size());
+        //OpenGLIndexBuffer* index_buffer = new OpenGLIndexBuffer(indices, 36);
 
         //vertex array object
         OpenGLVertexArrayObject vao;
         vao.AddVertexBuffer(vertex_buffer);
-        vao.AddVertexBuffer(color_buffer);
+        vao.AddVertexBuffer(normal_buffer);
         vao.SetIndexBuffer(index_buffer);
 
         vertex_buffer->Unbind();//To prove VAO works
-        color_buffer->Unbind();
+        normal_buffer->Unbind();
         index_buffer->Unbind();
-
+        
         glEnable(GL_DEPTH_TEST);//TODO: Create an wrapper to encapsulate RenderCommand ??
 
         glm::mat4* model = new glm::mat4(1.0);
-        *model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 0.0));
+        *model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 1.0f));
         glm::mat4* view = new glm::mat4(1.0f);
         glm::mat4* proj = new glm::mat4(1.0f);
 
         CameraManager* cam_mngr = CameraManager::GetInstance();
         PerspectiveCamera* cam = new PerspectiveCamera(1.0f * m_window->GetWidth(), 1.0f * m_window->GetHeight(), 0.1f, 100.0f);
         //OrthographicCamera cam2(-0.8f, 0.8f, -0.9, 0.9, -10, 10);
-        cam->SetPosition({ 0.0f, 0.0f, 3.0f });
+        cam->SetPosition({ 0.0f, 0.0f, -2.0f });
         //cam2.SetPosition({ 0.0f, 0.0f, 3.0f });
         
         //glm::vec4* light_pos = new glm::vec4(0.0f, 3.0f, 0.0f, 1.0f);
@@ -144,10 +169,16 @@ namespace Chroma
         shader->CreateUniform("u_View", ShaderDataType::Mat4, view);
         shader->CreateUniform("u_Proj", ShaderDataType::Mat4, proj);
         //shader->CreateUniform("u_LightPosition", ShaderDataType::Float4, light_pos);
+        glm::vec4 dir({ 0.0f, 0.0f, 1.0f, 0.0f });
+        float a = 0.04f;
 
         while (m_running)
         {
             *model = glm::rotate(*model, 0.03f, glm::vec3(0.0f, 1.0f, 0.3f));
+
+            dir = glm::rotate(glm::mat4(1.0f), a, glm::vec3(0.0f, 1.0f, 0.0f)) * dir;
+            //cam->SetDirection(dir);
+            //a += 0.01;
 
             *proj = cam->GetProjectionMatrix();
             *view = cam->GetViewMatrix();
@@ -165,7 +196,7 @@ namespace Chroma
 
         delete shader;
         delete vertex_buffer;
-        delete color_buffer;
+        delete normal_buffer;
         delete index_buffer;
 
     }
