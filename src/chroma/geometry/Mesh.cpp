e1d5b291c46945f60b4483be4080969453c8a728
Alper Şahıstan <alpersahistan@gmail.com>
2019-07-31 00:57:41 +0300

    Make some fields public

--- src/chroma/geometry/Mesh.cpp
+++ src/chroma/geometry/Mesh.cpp
@@ -3,17 +3,26 @@ namespace Chroma
 {
 
     Mesh::Mesh()
-    {}
+    {
+        m_bound_min = { std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max() };
+        m_bound_max = { std::numeric_limits<float>::min(), std::numeric_limits<float>::min(), std::numeric_limits<float>::min() };
+    }
 
     Mesh::Mesh(std::vector<glm::vec3> vertices, std::vector<glm::vec3> normals, std::vector<glm::vec2> texcoords, std::vector<glm::vec3> colors, unsigned int face_count)
     {
-        m_vertices = vertices;
-        m_normals = normals;
-        m_colors = colors;
-        m_texcoords = texcoords;
+        m_vertex_positions = vertices;
+        m_vertex_normals = normals;
+        m_vertex_colors = colors;
+        m_vertex_texcoords = texcoords;
 
         m_vertex_count = vertices.size();
         m_face_count = face_count;
+
+        m_bound_min = { std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max() };
+        m_bound_max = { std::numeric_limits<float>::min(), std::numeric_limits<float>::min(), std::numeric_limits<float>::min() };
+
+        CalculateBounds();
+        CenterToPivot();
     }
 
     void Mesh::CenterToPivot()
@@ -21,10 +30,10 @@ namespace Chroma
         glm::vec3 center(0, 0, 0);
 
         for (int i = 0; i < m_vertex_count; i++)
-            center += m_vertices[i] / (1.0f * m_vertex_count);
+            center += m_vertex_positions[i] / (1.0f * m_vertex_count);
 
         for (int i = 0; i < m_vertex_count; i++)
-            m_vertices[i] -= center;
+            m_vertex_positions[i] -= center;
 
         m_bound_max -= center;
         m_bound_min -= center;
@@ -39,13 +48,13 @@ namespace Chroma
         }
         else
         {
-            m_bound_min = m_vertices[0];
-            m_bound_max = m_vertices[0];
+            m_bound_min = m_vertex_positions[0];
+            m_bound_max = m_vertex_positions[0];
 
             for (int i = 0; i < m_vertex_count; i++)
             {
-                m_bound_min = glm::min(m_vertices[i], m_bound_min);
-                m_bound_max = glm::max(m_vertices[i], m_bound_max);
+                m_bound_min = glm::min(m_vertex_positions[i], m_bound_min);
+                m_bound_max = glm::max(m_vertex_positions[i], m_bound_max);
             }
         }
     }
--- src/chroma/geometry/Mesh.h
+++ src/chroma/geometry/Mesh.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <limits>
 #include <thirdparty/glm/glm/glm.hpp>
 #include <vector>
+
+
 namespace Chroma
 {
+#define VECTOR_FIELD_METHODS(VAR, NAME_S, NAME_P, TYPE) std::vector<TYPE> Get##NAME_P() const { return VAR; }\
+                                                    void Set##NAME_P(std::vector<TYPE> NAME) { VAR = NAME;}\
+                                                    void AddTo##NAME_P(TYPE val) { VAR.push_back(val);}\
+                                                    void ResizeSpaceFrom##NAME_P(unsigned int size) {VAR.resize(size);}\
+                                                    void Set##NAME_S##At(unsigned int index, TYPE val) {VAR.at(index) = val;}
 
     class Mesh
     {
@@ -16,12 +24,20 @@ namespace Chroma
         inline unsigned int GetVertexCount() const { return m_vertex_count; }
 
         inline void SetFaceCount(unsigned int f_c) { m_face_count = f_c; }
-        inline void GetVertexCount(unsigned int v_c) { m_vertex_count = v_c; }
+        inline void SetVertexCount(unsigned int v_c) { m_vertex_count = v_c; }
 
         inline glm::vec3 GetMaxBound() const { return m_bound_max; }
         inline glm::vec3 GetMinBound() const { return m_bound_min; }
 
-        inline std::vector<glm::vec3> GetVertices() const { return m_vertices; }
+        inline void SetMaxBound(glm::vec3 max_b) { m_bound_max = max_b; }
+        inline void SetMinBound(glm::vec3 min_b) { m_bound_min = min_b; }
+
+        /*VECTOR_FIELD_METHODS(m_vertex_positions, Position, Positions,glm::vec3);
+        VECTOR_FIELD_METHODS(m_vertex_normals, Normal, Normals, glm::vec3);
+        VECTOR_FIELD_METHODS(m_vertex_colors, Color, Colors, glm::vec3);
+        VECTOR_FIELD_METHODS(m_vertex_texcoords, TextureCoord, TextureCoords, glm::vec2);
+        VECTOR_FIELD_METHODS(m_indices, Index, Indices, int);*/
+        /*inline std::vector<glm::vec3> GetVertices() const { return m_vertices; }
         inline std::vector<glm::vec3> GetNormals() const { return m_normals; }
         inline std::vector<glm::vec3> GetColors() const { return m_colors; }
         inline std::vector<glm::vec2> GetTexCoords() const { return m_texcoords; }
@@ -29,8 +45,14 @@ namespace Chroma
         inline void SetVertices(std::vector<glm::vec3> vertices) { m_vertices = vertices; CalculateBounds(); CenterToPivot(); }
         inline void SetNormals(std::vector<glm::vec3> normals) { m_normals = normals; }
         inline void SetColors(std::vector<glm::vec3> colors) { m_colors = colors; }
-        inline void SetTexCoords(std::vector<glm::vec2> texcoords) { m_texcoords = texcoords; }
+        inline void SetTexCoords(std::vector<glm::vec2> texcoords) { m_texcoords = texcoords; }*/
 
+        std::vector<glm::vec3> m_vertex_positions;
+        std::vector<glm::vec3> m_vertex_normals;
+        std::vector<glm::vec3> m_vertex_colors;
+        std::vector<glm::vec2> m_vertex_texcoords;
+
+        std::vector<unsigned int> m_indices;
 
     private:
         void CenterToPivot();
@@ -41,10 +63,5 @@ namespace Chroma
 
         glm::vec3 m_bound_max;
         glm::vec3 m_bound_min;
-
-        std::vector<glm::vec3> m_vertices;
-        std::vector<glm::vec3> m_normals;
-        std::vector<glm::vec3> m_colors;
-        std::vector<glm::vec2> m_texcoords;
     };
 }
